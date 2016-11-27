fc4de655905fdc1a9e343dda01720e1701c24d14
David Kent <davidkent@fastmail.com.au>
2016-11-27 04:32:35 +0000

    A buffering sink.

--- sensor_feed/sink.py
+++ sensor_feed/sink.py
@@ -63,10 +63,44 @@ class DataFrameSink(Sink):
         print(self.df)
 
 
+class BufferedSink(Sink):
+    """
+        A buffered sink.
+
+        Tracks each sensor-series as a pd.Series. When the series exceeds
+        buf_length values it is passed to ``write_buffer``.
+
+        ``write_buffer`` must be implemented in a subclass.
+    """
+    def __init__(self, max_buffer=100):
+        self._buffers = {}
+        self.max_buffer = max_buffer
+
+    def process_value(self, param_name, timestamp, value):
+        """Handle a single datapoint."""
+        if param_name not in self._buffers:
+            self._buffers[param_name] = pd.Series()
+        self._buffers[param_name].ix[round_datetime(timestamp, 's')] = value
+
+        if len(self._buffers[param_name]) > self.max_buffer:
+            self.write_buffer(param_name, self._buffers[param_name])
+            del self._buffers[param_name]
+
+    def finalise(self):
+        for param_name in self._buffers:
+            self.write_buffer(param_name, self._buffers[param_name])
+
+
+class PrintingBufferSink(BufferedSink):
+    def write_buffer(self, param_name, series):
+        print(param_name, series)
+
+
 def sinks_from_config():
     """Create the sink objects."""
     LOGGER.critical('Starting sinks...')
     sinks = [LoggingSink()]
     if not NO_PANDAS:
         sinks.append(DataFrameSink())
+        sinks.append(PrintingBufferSink(max_buffer=5))
     return sinks
