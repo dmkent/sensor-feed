36a0e6d88cf9620942f200d4acbf91d35964841e
David Kent <davidkent@fastmail.com.au>
2017-01-07 10:07:52 +0000

    Add GPIO control to plant control sink.
    Now all in place and working. At lot of config could be moved out
    of static code.

--- sensor_feed/plant_control.py
+++ sensor_feed/plant_control.py
@@ -6,12 +6,13 @@ data to act on).
 import logging
 from threading import Lock, Timer
 
+from RPi import GPIO
 from sensor_feed.sensor import SleepingSensor
 from sensor_feed.sink import Sink
 
 
 LOGGER = logging.getLogger(__name__)
-
+GPIO.setmode(GPIO.BCM)
 
 class PlantControl(SleepingSensor, Sink):
     """A controller to water a plant."""
@@ -25,10 +26,13 @@ class PlantControl(SleepingSensor, Sink):
         super(SleepingSensor, self).__init__(*args, **kwargs)
         super(Sink, self).__init__(*args, **kwargs)
         self._water_input = 0
-        self.threshold = 6
-        self.water_period = 3
+        self.threshold = 1300
+        self.water_period = 20
         self.min_period = self.water_period + 2
         self._watering = Lock()
+        self.gpio_pin = 17
+
+        GPIO.setup(self.gpio_pin, GPIO.OUT)
 
     def get_value(self):
         last_water = self._water_input
@@ -40,27 +44,31 @@ class PlantControl(SleepingSensor, Sink):
         if param_name != self.trigger_param:
             return
 
-        if value < self.threshold:
+        if value > self.threshold:
             self.apply_water()
 
     def apply_water(self):
         LOGGER.critical('Applying water.')
         self._water_input += self.water_period
 
-        # Do something to turn on water supply.
-        # probably need to schedule something to turn it off again too...
         if self._watering.locked():
             LOGGER.critical('Already watering.')
             return
 
         self._watering.acquire()
+
+        # turn on water supply.
+        GPIO.output(self.gpio_pin, GPIO.HIGH)
+
         LOGGER.critical('Tap on.')
         timer = Timer(self.water_period, self._stop)
         timer.start()
 
     def _stop(self):
         LOGGER.critical('Tap off.')
+        GPIO.output(self.gpio_pin, GPIO.LOW)
         self._watering.release()
 
     def __del__(self):
+        GPIO.output(self.gpio_pin, GPIO.LOW)
         LOGGER.critical('Ensure tap off.')
